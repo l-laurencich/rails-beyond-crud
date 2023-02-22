e481b01def94d34ef8e5aa5c354236540ad102f8
lucas laurencich <lucas.maximiliano89@gmail.com>
2023-02-22 09:17:23 +0100

    seeded restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "cleaning the Database"
+
+Restaurant.destroy_all 
+
+puts "creating Restaurants"
+
+100.times do 
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name ,
+    address: Faker::Address.street_address,
+    rating: rand(1..5)
+  )
+  puts "Restaurant with #{restaurant.id} id was created"
+end
+
+puts "All done"
